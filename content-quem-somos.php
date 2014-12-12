85f3b0af1f3f4210d15826978035e78b8ec202b6
everaldomatias <matiaseveraldo@gmail.com>
2014-12-12 20:16:37 -0200

    Varios ajustes CSS

--- content-quem-somos.php
+++ content-quem-somos.php
@@ -7,20 +7,23 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	
 	<div id="esquerda-quem-somos">
-    <?php the_post_thumbnail(); ?>
+    	<?php the_post_thumbnail(); ?>
     </div><!-- #esquerda-quem-somos -->
     
    	<div id="direita-quem-somos">
-<header class="entry-header">
-		<h1 class="entry-title title-blog quicksand uppercase"><?php the_title(); ?></h1>
-	</header><!-- .entry-header -->
+	
+		<header class="entry-header">
+			<h1 class="entry-title title-blog quicksand uppercase"><?php the_title(); ?></h1>
+		</header><!-- .entry-header -->
 
-	<div class="entry-content">
-		<?php the_content(); ?>
-		<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'oqimporta' ), 'after' => '</div>' ) ); ?>
-	</div><!-- .entry-content -->
-	<?php edit_post_link( __( 'Editar', 'oqimporta' ), '<footer class="entry-meta"><span class="edit-link">', '</span></footer>' ); ?>
+		<div class="entry-content">
+			<?php the_content(); ?>
+			<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'oqimporta' ), 'after' => '</div>' ) ); ?>
+		</div><!-- .entry-content -->
+		
+		<?php edit_post_link( __( 'Editar', 'oqimporta' ), '<footer class="entry-meta"><span class="edit-link">', '</span></footer>' ); ?>
 
     </div><!-- #direita-quem-somos -->
     
